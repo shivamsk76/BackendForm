62f96787967219f850e1c782dd8bfa77db4d10cd
shivamsk76 <st7692833267@gmail.com>
2021-09-20 14:10:02 +0530

    shivam done

--- server.js
+++ server.js
@@ -1,37 +1,25 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const registration = require('./routes/Admission')
-const cors = require('cors')
-const dbConfig = require('./config/config')
-const ingestDataAddress = process.env.ALLOWEDADDRESS
+const express = require("express");
+const bodyParser = require("body-parser");
+const registration = require("./routes/Admission");
+const cors = require("cors");
+// const dbConfig = require('./config/config')
+const ingestDataAddress = process.env.ALLOWEDADDRESS;
 
+var app = express();
+app.use(bodyParser.json());
 
-var app = express()
-app.use(bodyParser.json())
+app.use(cors({ origin: "*" }));
 
+// dbConfig.authenticate().then(() => {
+//     console.log("database connected")
+// }).catch(err => console.log('Error: ' + err))
 
+app.post("/registration", registration);
 
+app.post("/chatapi", (req, res) => {
+  console.log("heelo there");
+});
 
-app.use(cors({ origin: ingestDataAddress }))
+const PORT = process.env.port || 8080;
 
-
-dbConfig.authenticate().then(() => {
-    console.log("database connected")
-}).catch(err => console.log('Error: ' + err))
-
-
-app.post('/registration', registration)
-
-app.post('/chatapi',(req,res)=>{
-    
-    console.log('heelo there')
-})
-
-
-
-
-
-const PORT = process.env.port || 8080
-
-
-app.listen(PORT, console.log("server is running", PORT))
+app.listen(PORT, console.log("server is running", PORT));
